0fb85050420a54b01b0c38947ffda65468c9bfef
Austin.Shepherd <Austin.Shepherd.96@gmail.com>
2019-01-08 17:43:26 -0800

    done.2

--- public/app.js
+++
@@ -1,3 +0,0 @@
-// might do this inline but try not to at first, create an ajax call to post the data from the comment field to the associated article
-//add function for button on click, scrape the new york times
-//add fucntion for the comment submit form
